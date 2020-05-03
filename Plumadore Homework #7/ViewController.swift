8a6295790875b229aeccc12774f44a6ac67c91ae
Ainsley Pluamdore <a.plumadore@setonhill.edu>
2020-05-03 11:24:23 -0400

    Initial Commit

---
+++ Plumadore Homework #7/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Plumadore Homework #7
+//
+//  Created by Ainsley Plumadore on 5/3/20.
+//  Copyright © 2020 Ainsley Plumadore. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Plumadore Homework #7Tests/Plumadore_Homework__7Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Plumadore_Homework__7Tests.swift
+//  Plumadore Homework #7Tests
+//
+//  Created by Ainsley Plumadore on 5/3/20.
+//  Copyright © 2020 Ainsley Plumadore. All rights reserved.
+//
+
+import XCTest
+@testable import Plumadore_Homework__7
+
+class Plumadore_Homework__7Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
